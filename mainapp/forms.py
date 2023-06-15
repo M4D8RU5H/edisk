335a5656a68d109dd231bc2476e8c6834c2c3370
madbrush <getkrzysiek200@gmail.com>
2023-06-15 15:05:26 +0200

    Add register and login features

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,5 +1,22 @@
 from django import forms
 from multiupload.fields import MultiFileField
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class NewUserForm(UserCreationForm):
+	email = forms.EmailField(required=True)
+
+	class Meta:
+		model = User
+		fields = ("username", "email", "password1", "password2")
+
+	def save(self, commit=True):
+		user = super(NewUserForm, self).save(commit=False)
+		user.email = self.cleaned_data['email']
+		if commit:
+			user.save()
+		return user
 
 class MyfileUploadForm(forms.Form):
     file_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="home"),
+    path('register', views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name= "logout")
 ]
